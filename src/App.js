11cf9cbb00d4d93f7b894de6d1faac7946eb895e
Levin-Alexey <levin.penza@yandex.ru>
2023-07-16 20:21:59 +0300

    Сделанно начальная верстка

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import Header from "./components/Header";
-import Tehnologies from "./components/Tehnologies";
+import Footer from "./components/Footer";
+import logo from "./files/images/logoMain.png";
 
 const App = () => {
-    return (<div>
-        <Header/>
-        <Tehnologies/>
+    return (<div className="app_wrapper">
+        <header className="header_main">
+            <img className="logoMain" src={logo} alt=""/>
+        </header>
+        <nav>
+            <ul>
+                <li>Profile</li>
+                <li>Messages</li>
+                <li>News</li>
+                <li>Music</li>
+                <li>Setting</li>
+            </ul>
+        </nav>
+        <section className="main_content">
+
+        </section>
+        <Footer/>
     </div>);
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
-const Header = () => {
+const footer = () => {
     return (<div className="header">
         <a href="#">Home</a>
         <a href="#">News Feed</a>
         <a href="#">Messages</a>
     </div>)};
 
-export default Header;
-
+export default footer;
