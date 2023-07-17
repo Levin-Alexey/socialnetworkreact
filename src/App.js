1f85442730325b723c0aeba0bbb5e628208a7f3f
Levin-Alexey <levin.penza@yandex.ru>
2023-07-17 11:52:57 +0300

    Готов разнос шаблона на Grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import './App.css';
-import Footer from "./components/Footer";
-import logo from "./files/images/logoMain.png";
+
 
 const App = () => {
-    return (<div className="app_wrapper">
-        <header className="header_main">
-            <img className="logoMain" src={logo} alt=""/>
-        </header>
-        <nav>
-            <ul>
-                <li>Profile</li>
-                <li>Messages</li>
-                <li>News</li>
-                <li>Music</li>
-                <li>Setting</li>
-            </ul>
-        </nav>
-        <section className="main_content">
-
-        </section>
-        <Footer/>
+    return (<div className="body">
+        <div className="app_wrapper">
+            <header className="header_wrapper">fdfdf</header>
+            <asaid className="left_menu_wrapper">sdsd</asaid>
+            <section className="user_content_wrapper">dfdfd</section>
+            <footer className="footer_wrapper">fdfd</footer>
+        </div>
     </div>);
 }
 
