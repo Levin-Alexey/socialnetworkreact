ae8c117db52226810c4a181c33fcbd4e3e5aec60
Levin-Alexey <levin.penza@yandex.ru>
2023-07-22 13:23:09 +0300

    Добавил компоненту Post

---
+++ src/components/Post.js
@@ -0,0 +1,9 @@
+import classes from '../css/Post.module.css';
+
+const Post = (props) => {
+    return <div className={classes.post_wrapper}>
+        <div ></div>
+    </div>
+};
+
+export default Post;
--- src/components/UserContent.js
+++ src/components/UserContent.js
@@ -1,12 +1,18 @@
 import classes from '../css/UserContent.module.css';
+import Post from "./Post";
+
+
 const UserContent = () => {
     return <section className={classes.user_content_wrapper}>
         <div className={classes.user_info_wrapper}>
             <div className={classes.user_avata}></div>
             <div className={classes.user_info}></div>
         </div>
-        <div className={classes.user_posts_wrapper}></div>
         <div className={classes.new_post_wrapper}></div>
+        <div className={classes.user_posts_wrapper}>
+            <Post/>
+        </div>
+
     </section>
 }
 export default UserContent;
